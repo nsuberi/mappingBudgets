43fbfff9ee07f231b58609b69090d9fc54cb8347
NSuberi <nsuberi@gmail.com>
2016-11-23 11:37:46 -0500

    security update

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
-
-var conn = "postgres://peimqntngkgvkj:Il061m86mRi9CsunKN1V-EUf4u@ec2-54-243-199-79.compute-1.amazonaws.com:5432/dbhnujh4tn8nt8";
 module.exports = process.env.DATABASE_URL || conn;
 
 //'postgres://postgres:admin@localhost/todo';
